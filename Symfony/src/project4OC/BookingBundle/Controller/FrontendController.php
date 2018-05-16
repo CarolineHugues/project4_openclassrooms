9516c0570bedc8df70d880ec3660dcf6981e5196
Caroline Hugues <hugues.caroline9@gmail.com>
2018-05-16 19:21:09 +0200

    Ajout des formulaires imbriqués

--- Symfony/src/project4OC/BookingBundle/Controller/FrontendController.php
+++ Symfony/src/project4OC/BookingBundle/Controller/FrontendController.php
@@ -7,10 +7,38 @@ namespace project4OC\BookingBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
+use project4OC\BookingBundle\Entity\Ticket;
+use project4OC\BookingBundle\Entity\Booking;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use project4OC\BookingBundle\Form\TicketType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class FrontendController extends Controller 
 {
 	public function indexAction()
 	{
-		return $this->render('project4OCBookingBundle:Frontend:index.html.twig', array('page_title' => 'Réservation - Musée du Louvre'));
+    $booking = new Booking();
+
+    $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $booking);
+
+    $formBuilder 
+      ->add('ticketType',      RadioType::class)
+      ->add('mail',            EmailType::class)
+      ->add('numberOfTickets', IntegerType::class)
+      ->add('tickets',         CollectionType::class, array(
+        'entry_type'   =>      TicketType::class,
+        'allow_add'    => true,
+        'allow_delete' => true
+      ))
+      ->add('save',            SubmitType::class);
+    ;
+
+    $form = $formBuilder->getForm();
+
+		return $this->render('project4OCBookingBundle:Frontend:index.html.twig', array('page_title' => 'Réservation en ligne', 'form' => $form->createView(),));
 	}
 }
