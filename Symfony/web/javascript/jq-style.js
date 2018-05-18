9ebd8de777a9e56420d62d66bf076ac176437101
Caroline Hugues <hugues.caroline9@gmail.com>
2018-05-18 10:25:58 +0200

    Ajout du script permettant d'ajouter et supprimer des tickets dans le formulaire de réservation

--- Symfony/web/javascript/jq-style.js
+++ Symfony/web/javascript/jq-style.js
@@ -1,3 +1,55 @@
+// Présentation des informations pratiques sous forme d'onglets
 $( function() {
     $( "#tabs" ).tabs();
- } );
+} );
+
+// Pouvoir ajouter et supprimer des tickets dans le formulaire de réservation selon le nombre indiqué
+$( function() {
+    var $container = $('div#booking_tickets');
+
+    var index = $container.find(':input').length;
+
+    $('#booking_numberOfTickets').change(function(e) 
+    {
+    	var numberTickets = $("#booking_numberOfTickets").val(); 
+    	
+    	if (index < numberTickets)
+    	{
+    		while(index < numberTickets)
+    		{
+    			addTicket($container);
+    		}
+   		}
+   		else
+    	{
+    		while(index > numberTickets)
+   			{
+    			deleteTicket();
+    		}
+   		}
+   	});
+
+
+    function addTicket($container) {
+
+      var template = $container.attr('data-prototype')
+        .replace(/__name__label__/g, 'Ticket n°' + (index+1))
+        .replace(/__name__/g,        index)
+      ;
+
+      var $prototype = $(template);
+
+      $container.append($prototype);
+
+      index++;
+    }
+
+    function deleteTicket() {
+	
+      var $ticketToDelete = $("#booking_tickets_" + (index-1)).parent();
+
+      $ticketToDelete.remove();
+
+      index--;
+    }  	
+});
