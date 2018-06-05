4378343b691868aead6f03457b39ecc3cb93d2e8
Caroline Hugues <hugues.caroline9@gmail.com>
2018-06-05 16:37:41 +0200

    Déplacement d'une fonction dans le service VerifyAvailableDate

--- Symfony/src/project4OC/BookingBundle/Entity/BookingManager.php
+++ Symfony/src/project4OC/BookingBundle/Entity/BookingManager.php
@@ -16,39 +16,7 @@ class BookingManager
 
     	return $numberOfTickets;
     }
-
-    public function individualOrGroupRate($numberOfWhishedTickets)
-    {
-		if ($numberOfWhishedTickets < 15)
-		{
-			return $numberOfWhishedTickets;
-		}
-		else
-		{
-			return $message = "Veuillez contacter l'équipe du musée pour réserver et avoir des renseignements à propos de notre tarif groupe.";
-		}
-	}
-
-	public function notEnoughTickets($numberOfWhishedTickets, VisitDay $visitDay)
-	{
-		$gauge = $visitDay->getGauge();
-		$numberAvailableTickets = (1000 - $gauge);
-		if ($numberAvailableTickets > $numberOfWhishedTickets)
-		{
-			return $gaugeAlmostReached = false;
-		}
-		else 
-		{	if ($numberAvailableTickets <= 1)
-			{
-				return $message = "Il reste seulement " . $numberAvailableTickets . " ticket disponible à la date choisie.";
-			}
-			else
-			{
-				return $message = "Il reste seulement " . $numberAvailableTickets . " tickets disponibles à la date choisie.";
-			}	
-		}
-	}
-
+	
 	public function computeTotalPrice(Booking $booking)
 	{
 		$totalPrice = 0;
