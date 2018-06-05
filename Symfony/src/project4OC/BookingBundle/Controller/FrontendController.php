31d8b11e2b0d560d54cb587f542fca5f321e9184
Caroline Hugues <hugues.caroline9@gmail.com>
2018-06-05 16:41:36 +0200

    Ajout du formatage de la date pour la base de donnée

--- Symfony/src/project4OC/BookingBundle/Controller/FrontendController.php
+++ Symfony/src/project4OC/BookingBundle/Controller/FrontendController.php
@@ -19,6 +19,12 @@ class FrontendController extends Controller
 
     	$form = $this->get('form.factory')->create(BookingType::class, $booking);
 
+    	if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) 
+    	{
+			$formatDate = $this->container->get('project4_oc_booking.formatdate');
+			$date =  $formatDate->formatVisitDayDateToDb($booking);
+       	}
+
 		return $this->render('project4OCBookingBundle:Frontend:index.html.twig', array('page_title' => 'Réservation en ligne', 'form' => $form->createView(),));
 	}
 }
