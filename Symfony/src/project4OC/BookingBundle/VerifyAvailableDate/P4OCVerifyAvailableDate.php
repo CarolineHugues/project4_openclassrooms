2a0bbac9881dcce20b2bb2aa78f36f5dc892abf3
Caroline Hugues <hugues.caroline9@gmail.com>
2018-06-30 11:16:12 +0200

    Correction service verifyAvialableDate

--- Symfony/src/project4OC/BookingBundle/VerifyAvailableDate/P4OCVerifyAvailableDate.php
+++ Symfony/src/project4OC/BookingBundle/VerifyAvailableDate/P4OCVerifyAvailableDate.php
@@ -53,7 +53,7 @@ class P4OCVerifyAvailableDate
 			$currentTime = explode('/', date('i/h'));
 			$currentDate = explode ('/', date('w/d/m/Y'));
 
-			if ($selectedDate[3] < $currentDate[3] OR (($selectedDate[3] == $currentDate[3]) AND  ($selectedDate[2] < $currentDate[2])) OR (($selectedDate[3] == $currentDate[3]) AND  ($selectedDate[2] == $currentDate[2]) AND ($selectedDate[1] < $currentDate[1])) OR $gauge >= 1000 OR $selectedDate[0] == 2 OR ($selectedDate[1] == 1 AND $selectedDate[2] == 05) OR ($selectedDate[1] == 1 AND $selectedDate[2] ==  11) OR ($selectedDate[1] == 25 AND $selectedDate[2] == 12) OR ($selectedDate == date('w/d/m/Y') AND $currentTime[1] > 18))
+			if ($selectedDate[3] < $currentDate[3] OR (($selectedDate[3] == $currentDate[3]) AND  ($selectedDate[2] < $currentDate[2])) OR (($selectedDate[3] == $currentDate[3]) AND  ($selectedDate[2] == $currentDate[2]) AND ($selectedDate[1] < $currentDate[1])) OR $gauge >= 1000 OR $selectedDate[0] == 2 OR ($selectedDate[1] == 1 AND $selectedDate[2] == 05) OR ($selectedDate[1] == 1 AND $selectedDate[2] ==  11) OR ($selectedDate[1] == 25 AND $selectedDate[2] == 12) OR ($selectedDate == date('w/d/m/Y') AND $currentTime[1] >= 18))
 			{
 				return $message = 'Cette date n\'est pas disponible à la réservation';
 			}
@@ -74,7 +74,7 @@ class P4OCVerifyAvailableDate
 		$selectedDateText = $selectedDate->format('d/m/y');
 		$currentTime = explode('/', date('i/h'));
 
-		if ($selectedDateText == date('d/m/Y') AND $currentTime[1] > 14)
+		if ($selectedDateText == date('d/m/Y') AND $currentTime[1] >= 14)
 		{
 			return $message = 'Il n\'est pas possible de réserver le billet "Journée" pour le jour même une fois 14 heures passées.';
 		}
